cb87c97d71cb05d3d469d98c1b91b8f651df25d2
betousqueda1974 <betousqueda@gmail.com>
2025-06-30 20:39:16 -0300

    Se saca toda la parte Administrativa a un Context.

--- src/assets/layouts/Admin.jsx
+++ src/assets/layouts/Admin.jsx
@@ -1,102 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import AltaProducto from "../components/nofijos/AltaProducto";
 import EditarProducto from "../components/nofijos/EditarProducto";
+import { AdminContext } from "../../context/AdminContext";
 
 const Admin = () => {
-    const [productos, setProductos] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [open, setOpen] = useState(false)
-    const [seleccionado, setSeleccionado] = useState(null)
-    const [openEditor, setOpenEditor] = useState(false)
 
-    useEffect(() => {
-        fetch("https://685e9e657b57aebd2afa198d.mockapi.io/ecomm-products/productos")
-            .then((response) => response.json())
-            .then((data) => {
-                setTimeout(() => {
-                    setProductos(data);
-                    setLoading(false);
-                }, 2000);
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-                setError(true);
-                setLoading(false);
-            });
-    }, []);
-
-
-    const cargarProductos = async()=>{
-        try {
-            const res = await fetch("https://685e9e657b57aebd2afa198d.mockapi.io/ecomm-products/productos")
-            const data = await res.json()
-            setProductos(data)
-        } catch (error) {
-            console.log('Error al cargar productos ', error);
-            
-        }
-    }
-
-
-    const agregarProducto = async (producto) =>{
-        try{
-            const respuesta = await fetch('https://685e9e657b57aebd2afa198d.mockapi.io/ecomm-products/productos',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(producto)
-        })
-        if(!respuesta.ok){
-            throw new Error('Error al agregar producto')
-        }
-        const data = await respuesta.json()
-        alert('Producto agregado correctamente')
-        cargarProductos()
-        }catch(error){
-            console.log(error.message);
-            
-        }
-    }
-
-
-    const actualizarProducto = async(producto) =>{
-        try {
-              const respuesta = await fetch(`https://685e9e657b57aebd2afa198d.mockapi.io/ecomm-products/productos/${producto.id}`,
-                {method:'PUT',
-                    headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(producto)
-                })
-                if(!respuesta.ok) throw Error ('Error al actualizar el producto')
-                    const data = await respuesta.json()
-                alert('Producto actualizado correctamente')
-                setOpenEditor(false)
-                setSeleccionado(null)
-                cargarProductos()
-        } catch (error) {
-            console.log(error.message);
-            
-        }
-    }
-
-
-    const eliminarProducto = async (id)=>{
-      const confirmar = window.confirm('Estas seguro de eliminar el producto?')
-      if (confirmar) {
-        try{
-          const respuesta = await fetch(`https://685e9e657b57aebd2afa198d.mockapi.io/ecomm-products/productos/${id}`,{
-          method:'DELETE',
-          })
-            if(!respuesta.ok) throw Error('Error al eliminar')
-              alert('Producto Eliminado correctamente')
-              cargarProductos()
-            }catch(error){
-               alert('Hubo un problema al eliminar el producto')
-            }
-        }
-    }
+    const {productos,
+            loading,
+            open,
+            setOpen,
+            openEditor,
+            setOpenEditor,
+            seleccionado,
+            setSeleccionado,
+            agregarProducto,
+            actualizarProducto,
+            eliminarProducto,
+        } = useContext(AdminContext) 
 
     return (
         <div className="container">
@@ -129,13 +49,11 @@ const Admin = () => {
                                 <span>{product.nombre}</span>
                                 <span>${product.precio}</span>
                                 <div>
-                                    {/* <button className="edi tButton">Editar</button> */}
                                     <button className="editButton" onClick={()=>{
                                         setOpenEditor(true)
                                         setSeleccionado(product)
                                     }}>Editar</button>
 
-                                    {/* <button className="deleteButton">Eliminar</button> */}
                                     <button className="deleteButton" onClick={()=> eliminarProducto(product.id)}>Eliminar</button>
                                 </div>
                             </li>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
+import { AdminProvider } from './context/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>
-      <App />
+      <AdminProvider>
+        <App />
+      </AdminProvider>
     </CartProvider>
   </StrictMode>,
 )
